363cfbd80d1149547f12cb561cc667d54d407cb2
unknown <bidjokaalexandre@gmail.com>
2022-06-14 20:53:46 +0200

    correction de la mise à jour

--- webServices/FormationRestApi/src/main/java/formation/rest/ConnexionRest.java
+++ webServices/FormationRestApi/src/main/java/formation/rest/ConnexionRest.java
@@ -1,6 +1,10 @@
 package formation.rest;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,8 +23,13 @@ public class ConnexionRest {
 	private ConnexionBLL bll;
 	
 	@GetMapping
-	public List<Utilisateur> getUtilisateurs() {
-		return bll.selectAll();
+	public ResponseEntity<List<Utilisateur>> getAllUtilisateurs() {
+		try {
+			return new ResponseEntity<List<Utilisateur>>(bll.selectAll(), HttpStatus.OK);
+		}
+		catch (Exception e) {
+			return new ResponseEntity<List<Utilisateur>>(HttpStatus.CONFLICT);
+		}
 	}
 	
 	@GetMapping("/{login}/{password}")
